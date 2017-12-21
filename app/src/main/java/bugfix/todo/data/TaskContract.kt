aabcdc68567938ff0e69792f50ca3475093ac063
Shicselov A <pyro-bot@yandex.ru>
2017-12-21 17:12:43 +0500

    добавлены комментарии в файл описание контента

--- app/src/main/java/bugfix/todo/data/TaskContract.kt
+++ app/src/main/java/bugfix/todo/data/TaskContract.kt
@@ -5,29 +5,41 @@ import android.content.ContentUris
 import android.net.Uri
 import android.provider.BaseColumns
 import java.net.URI
-
-/**
- * Created by a_shicelov on 13.12.2017.
- */
-
+//Обшие описание контента для приложения
 object TaskContract {
+//    Кому принадлежит содержимое (полное название основного пакета)
     val CONTENT_AUTHORITY = "bugfix.todo"
+//    Создание корневого URI для нашего приложения
     val BASE_CONTENT_URI: Uri = Uri.parse("content://${CONTENT_AUTHORITY}")
+//    Часть пути относильно корня для получения списка задач, без знака / в начале и в конце
     val TASK_PATH = "${TaskEntry.TABLE_NAME}"
 
+//    Описание отдельной сущности и таблицы
     object TaskEntry {
 
-        val CONTENT_URI: Uri = Uri.parse("${BASE_CONTENT_URI.toString()}/${TASK_PATH}")//BASE_CONTENT_URI.buildUpon().appendPath(TASK_PATH).build()
+//    URI для получения списка всех задач
+        val CONTENT_URI: Uri = Uri.parse("${BASE_CONTENT_URI.toString()}/${TASK_PATH}")
+//    Тип контента
         val CONTENT_TYPE = "${ContentResolver.CURSOR_DIR_BASE_TYPE}/${CONTENT_AUTHORITY}/${TASK_PATH}"
+//    Тип отдельного элимента
         val CONTENT_ITEM_TYPE = "${ContentResolver.CURSOR_ITEM_BASE_TYPE}/${CONTENT_AUTHORITY}/${TASK_PATH}"
 
+//    Название таблице
         val TABLE_NAME = "tasks"
 
+//    ===============================
+//    Стандартные поля
+//    Название поля ID
         val _ID = BaseColumns._ID // Y U NO implement?!
+//    Название поля COUNT
         val _COUNT = BaseColumns._COUNT
+//    ===============================
+//    Название поля с названием задачи
         val COL_TITLE = "title"
+//    Название поля с описанием задачи
         val COL_DESCRIPTION = "description"
 
+//    Метода построения URI для нужного id и его получения с URI
         fun buildWithId(id: Long): Uri {
             return ContentUris.withAppendedId(CONTENT_URI, id)
         }
